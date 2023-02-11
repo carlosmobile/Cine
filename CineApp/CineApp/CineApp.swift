d6d1e4ae85f7ff945fcc9016a88170b4fade03fe
CarlosMobile <revealdottedline@icloud.com>
2023-02-11 13:48:14 +0100

    Add empty swiftui base project

---
+++ CineApp/CineApp/CineApp.swift
@@ -0,0 +1,17 @@
+//
+//  CineApp.swift
+//  CineApp
+//
+//  Created by Carlos on 11/2/23.
+//
+
+import SwiftUI
+
+@main
+struct CineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
