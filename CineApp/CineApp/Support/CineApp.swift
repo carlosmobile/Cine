34cd48db5ad282876d7041e6a95f5bf183ac4970
CarlosMobile <revealdottedline@icloud.com>
2023-02-12 13:08:00 +0100

    Add appdelegate to swiftui project

--- CineApp/CineApp/Support/CineApp.swift
+++ CineApp/CineApp/Support/CineApp.swift
@@ -9,9 +9,28 @@ import SwiftUI
 
 @main
 struct CineApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    @Environment(\.scenePhase) private var scenePhase
+    
     var body: some Scene {
         WindowGroup {
             MovieListView()
+        }
+        
+        WindowGroup {
+            MovieListView()
+        }
+        .onChange(of: scenePhase) { (newScenePhase) in
+            switch newScenePhase {
+            case .active:
+                print("The scene is in the foreground and interactive.")
+            case .inactive:
+                print("The scene is in the foreground but should pause its work.")
+            case .background:
+                print("The scene isn't currently visible in the UI.")
+            @unknown default:
+                break
+            }
         }
     }
 }
