091eaef570aaa2fefbea7018e9e53363a258a243
CarlosMobile <revealdottedline@icloud.com>
2023-02-12 13:28:52 +0100

    Add json origin data and datamanager to parse json to models

--- CineApp/CineApp/Network/DataManager.swift
+++ CineApp/CineApp/Network/DataManager.swift
@@ -5,4 +5,37 @@
 //  Created by Carlos on 11/2/23.
 //
 
-import Foundation
+import SwiftUI
+
+struct Constants {
+    static let baseURL = "https://media.tvup.cloud"
+}
+
+class DataManager: ObservableObject  {
+    var showsByGenre = [ShowsByGenre]()
+    
+    init() {
+        loadData()
+    }
+    
+    func loadData() {
+        guard let url = Bundle.main.url(forResource: "last7d.cine", withExtension: "json")
+            else {
+                print("json file not found")
+                return
+            }
+        
+        guard let dataFromJSON = try? Data(contentsOf: url) else { return }
+        guard let showsByGenre = try? JSONDecoder().decode([ShowsByGenre].self, from: dataFromJSON) else { return }
+        self.showsByGenre = showsByGenre
+    }
+    
+    func getAllMoviesFromShowsByGenre() -> [Movie] {
+        var movies: [Movie] = []
+        showsByGenre.forEach { showsByGenre in
+            movies += showsByGenre.movies
+        }
+        
+        return movies
+    }
+}
